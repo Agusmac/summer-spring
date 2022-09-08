634be41663c63808e97814b5c09011964e01b329
Agusmac <agusmac40@gmail.com>
2022-09-08 14:02:07 -0300

    removed animated cursor on mobile

--- pages/index.js
+++ pages/index.js
@@ -15,20 +15,21 @@ export default function Home() {
 
   const [openInk, setOpenInk] = useState(true)
   const [openSidebar, setOpenSidebar] = useState(true)
-  
 
   // bg-slate-500 md:bg-[#db7f5a] lg:bg-[#fcf9ee]
   return (
     <>
-      <div className='bg-[#fcf9ee] min-h-screen '>
-        <Hero setOpenInk={setOpenInk}/>
+      <div className='bg-[#fcf9ee] min-h-screen overflow-x-hidden md:cursor-none'>
+        <Hero setOpenInk={setOpenInk} />
         <YellowBand />
         <Whoweare />
         <ItemLine />
         <Explore text='SHOP ALL' />
         <DoubleReview />
-        <Footer setOpenSidebar={setOpenSidebar}/>
-        <Goo />
+        <Footer setOpenSidebar={setOpenSidebar} />
+        <div className='hidden md:block'>
+          <Goo />
+        </div>
         <Layer openInk={openInk} openSidebar={openSidebar} />
       </div>
       <Loader />
